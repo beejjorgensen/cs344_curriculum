505e11f33575725f72437ac761832e702351231d
Brian 'Beej Jorgensen' Hall <beej@beej.us>
2022-02-10 18:16:24 -0800

    add alternate output

--- week06/project/myalloc.c
+++ week06/project/myalloc.c
@@ -34,7 +34,8 @@ void print_data(void)
     }
 
     while (b != NULL) {
-        printf("[%p:%d,%s]", b, b->size, b->in_use? "used": "free");
+        //printf("[%p:%d,%s]", b, b->size, b->in_use? "used": "free");
+        printf("[%d,%s]", b->size, b->in_use? "used": "free");
         if (b->next != NULL) {
             printf(" -> ");
         }
@@ -49,6 +50,8 @@ void print_data(void)
 static int get_break_size(int size)
 {
     long page_size = sysconf(_SC_PAGESIZE);
+
+    page_size = 1024;
 
     // Assumes page_size is a power of 2
 
@@ -237,6 +240,7 @@ void mystrategy(enum strategy s)
 
 int main(void)
 {
+#if 0
     void *p, *q;
 
     p = myalloc(512);
@@ -262,4 +266,14 @@ int main(void)
 
     myfree(r);
     print_data();
+#endif
+
+    void *p;
+
+    myalloc(10);     print_data();
+    p = myalloc(20); print_data();
+    myalloc(30);     print_data();
+    myfree(p);       print_data();
+    myalloc(40);     print_data();
+    myalloc(10);     print_data();
 }
